0bda5edb15572fa0d26eb45c38e0e597a8b94ad8
shahilmohamed <shahil.m.mohamed@gmail.com>
2025-05-22 17:43:37 +0530

    Medical history page initiated

--- src/app/medical-history/medical-history.component.ts
+++ src/app/medical-history/medical-history.component.ts
@@ -1,21 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-medical-history',
-  template: `<h3>Medical History for Patient ID: {{ patientId }}</h3>`,
   templateUrl: './medical-history.component.html',
   styleUrls: ['./medical-history.component.css']
 })
 export class MedicalHistoryComponent implements OnInit {
 
-  patientId: number=0;
-  name:string="Hello";
+  patientId: number = 0;
+  name: string = "";
+  selectedPrescription: any=null;
 
-  constructor(private route: ActivatedRoute) {}
+  history = [
+    {
+      diagnosisDate: '2024-12-01',
+      diagnosis: 'Flu',
+      revisitDate: '2024-12-10',
+      review: 'Mild improvement',
+      prescription: [
+        { drug: 'Paracetamol', morning: true, afternoon: false, night: true, days: 5 },
+        { drug: 'Antibiotic A', morning: true, afternoon: true, night: true, days: 7 }
+      ]
+    },
+    {
+      diagnosisDate: '2025-01-15',
+      diagnosis: 'Cold and cough',
+      revisitDate: '2025-01-25',
+      review: 'Fully recovered',
+      prescription: [
+        { drug: 'Cough Syrup', morning: false, afternoon: true, night: true, days: 3 }
+      ]
+    }
+  ];
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.patientId = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+   openPrescription(prescription: any): void {
+    this.selectedPrescription = prescription;
+    const modal = document.getElementById('prescriptionModal');
+    if (modal) new bootstrap.Modal(modal).show();
+  }
+
+  value = '';
+  filteredHistory = [...this.history];
+
+  onClickSearch(value: string): void {
+    const query = value.trim().toLowerCase();
+
+    this.filteredHistory = this.history.filter(patient => {
+      return (
+        patient.diagnosis.toLowerCase().includes(query) 
+      );
+    });
+    if (!query) {
+      this.filteredHistory = [...this.history];
+      return;
+    }
+
   }
 
 }
