3018585cf4a6c9d92d42242d8219c7376089bf7a
shahilmohamed <shahil.m.mohamed@gmail.com>
2025-09-12 01:45:17 +0530

    Issue #31 | Implemented auth guard successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -9,14 +7,10 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
-
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CookieService } from 'ngx-cookie-service';
+import { BrowserModule } from '@angular/platform-browser';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,11 +24,6 @@ import { CookieService } from 'ngx-cookie-service';
     FormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatNativeDateModule,
-    MatFormFieldModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgxPaginationModule
